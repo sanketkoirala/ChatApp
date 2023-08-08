27b79ae5272b346d8763e92676f1da9f5a0c41a7
Sanket Koirala <koiralasanket11@gmail.com>
2023-08-07 23:51:56 -0500

    kbhit library function

--- kbhit.c
+++ kbhit.c
@@ -1,3 +1,30 @@
-//
-// Created by Sanket Koirala on 8/5/23.
-//
+#include <stdio.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/time.h>
+#include <sys/select.h>
+
+
+#include "kbhit.h"
+
+
+// returns true if keyboard has been hit and the key is in input buffer
+bool kbhit(void)
+{
+    bool success;
+    // Look for data
+
+    fd_set fd;
+    struct timeval tv;
+    tv.tv_sec = 0;
+    tv.tv_usec = 0;
+    FD_ZERO(&fd);
+    FD_SET(0, &fd);
+    select(1, &fd, NULL, NULL, &tv);
+    success = FD_ISSET(0, &fd);
+    return success;
+}
+
+
